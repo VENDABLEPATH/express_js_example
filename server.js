43f1b9f6613223370a44111933edafc9da48f6d3
VENDABLEPATH <wianjacobs04@gmail.com>
2024-02-26 13:04:22 +0200

    Renamed file index to server

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const app = express();
 
 const PORT = 3000;
 
+app.get(`/`, (req, res) => {
+    res.send(`Page loaded.`);
+});
+
+app.get(`/messages`, (req, res) => {
+    res.send(`<p>Hello albert.</p>`);
+});
+
+app.post('/messages', (req, res) => {
+    console.log(`updating messages`);
+});
+
+
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`);
 });
